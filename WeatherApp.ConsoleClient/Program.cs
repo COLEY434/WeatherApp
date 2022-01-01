49c11cf01546b1623fce9eb42a064f8690e42961
collins Anieze <chigoziecollins434@gmail.com>
2022-01-01 22:32:08 +0100

    Added input validation for the select option 1

--- WeatherApp.ConsoleClient/Program.cs
+++ WeatherApp.ConsoleClient/Program.cs
@@ -6,7 +6,78 @@ namespace WeatherApp.ConsoleClient
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine($"Welcome to world Weather Information centre.{Environment.NewLine}");
+            DisplayWeatherOption();
+
+            string input;
+            while (true)
+            {
+                input = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(input))
+                {
+                    DisplayWeatherOption();
+                    continue;
+                }              
+
+
+                if(int.TryParse(input, out int value))
+                {
+                    if(value == 1)
+                    {
+                        Console.WriteLine("Type a country name");
+                        var country = Console.ReadLine();
+
+                        if (!string.IsNullOrWhiteSpace(country))
+                        {
+                            Console.WriteLine("Type a city name");
+                            string city = Console.ReadLine();
+
+                            if (!string.IsNullOrWhiteSpace(city))
+                            {
+
+                            }
+                            else
+                            {
+                                DisplayWeatherOption();
+                                continue;
+                            }
+
+                        }
+                        else
+                        {
+                            DisplayWeatherOption();
+                            continue;
+                        }
+                    }
+                    else if(value == 2)
+                    {
+                        Console.WriteLine("Option two selected");
+                    }
+                    else
+                    {
+                        DisplaySelectOptionMessage();
+                    }
+                }
+                else
+                {
+                    DisplaySelectOptionMessage();
+                }
+
+                DisplayWeatherOption();
+            }
+
+            
+        }
+
+        private static void DisplayWeatherOption()
+        {
+            Console.WriteLine($"Please select an option below...{Environment.NewLine}1) For real-time weather information.{Environment.NewLine}2) For periodic weather information.\n");
+        }
+
+        private static void DisplaySelectOptionMessage()
+        {
+            Console.WriteLine("Please select an option");
         }
     }
 }
